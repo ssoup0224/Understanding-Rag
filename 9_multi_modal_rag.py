15e7537a0ab7ad42496fd99c105694883c72ecbc
정성국 <ssoup0224@gmail.com>
2026-01-19 21:07:29 +0900

    added chunk_by_title method

--- 9_multi_modal_rag.py
+++ 9_multi_modal_rag.py
@@ -45,6 +45,22 @@ def partition_document(file_path: str):
     print(f"Extracted {len(elements)} elements")
     return elements
 
+# Step 2: Chunking by title
+def create_chunks_by_title(elements):
+    """Create intelligent chunks using title-based strategy"""
+    print("🔨 Creating smart chunks...")
+    
+    chunks = chunk_by_title(
+        elements, # The parsed PDF elements from previous step
+        max_characters=3000, # Hard limit - never exceed 3000 characters per chunk
+        new_after_n_chars=2400, # Try to start a new chunk after 2400 characters
+        combine_text_under_n_chars=500 # Merge tiny chunks under 500 chars with neighboring
+    )
+    
+    print(f"Created {len(chunks)} chunks")
+    return chunks
+
+
 if __name__ == "__main__":
     file_path = "./docs/attention-is-all-you-need.pdf"
     elements = partition_document(file_path)
@@ -53,5 +69,15 @@ if __name__ == "__main__":
     # print(set([str(type(el)) for el in elements])) # Different types of element types extracted from the PDF
     # print(elements[36].to_dict()) # use to_dict() method to see all attributes of an element
     images = [element for element in elements if element.category == 'Image'] # Filter only image elements
-    print(f"Extracted {len(images)} images") # Number of images extracted
+    # print(f"Extracted {len(images)} images") # Number of images extracted
     # print(images[0].to_dict()) # Print first image element details
+    tables = [element for element in elements if element.category == 'Table'] # Filter only table elements
+    # print(f"Extracted {len(tables)} tables") # Number of tables extracted
+    # print(tables[0].to_dict()) # Print first table element details
+    # Create chunks
+    chunks = create_chunks_by_title(elements)
+    # print(set([str(type(chunk)) for chunk in chunks]))  # Different types of chunks created
+    # print(chunks[0].to_dict())  # Print first chunk details
+    # print(chunks[4].metadata.orig_elements) # View original elements associated with the chunk
+    # Note: 4th chunk has the first image + 11th chunk has the first table in the sample PDF
+    
